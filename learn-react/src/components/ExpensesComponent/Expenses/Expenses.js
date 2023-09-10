f9ac8b1b668f0fead6b161b3d5276324a771b54d
yashUcr773 <yagga004@ucr.edu>
2023-09-10 11:38:15 +0530

    lifting up state / child to parent communication

--- learn-react/src/components/ExpensesComponent/Expenses/Expenses.js
+++ learn-react/src/components/ExpensesComponent/Expenses/Expenses.js
@@ -1,14 +1,34 @@
+// module imports
+
+import React, { useState } from "react";
+
+// component imports
 import ExpenseItems from "../ExpenseItem/ExpenseItem";
 import CardWrapper from "../../UI/CardWrapper/CardWrapper";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+
+// style imports
 import "./Expenses.scss";
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState("2020");
+
+    const getExpenseFilterValue = (value) => {
+        setFilteredYear(value);
+    };
+
     return (
-        <CardWrapper className="expenses">
-            {props.expenses.map((expense) => {
-                return <ExpenseItems expense={expense} key={expense.id} />;
-            })}
-        </CardWrapper>
+        <div>
+            <CardWrapper className="expenses">
+                <ExpenseFilter
+                    filteredYear={filteredYear}
+                    getExpenseFilterValue={getExpenseFilterValue}
+                />
+                {props.expenses.map((expense) => {
+                    return <ExpenseItems expense={expense} key={expense.id} />;
+                })}
+            </CardWrapper>
+        </div>
     );
 }
 
