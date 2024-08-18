4776908c9523a499bfa057163e2c6baee609de21
yashUcr773 <yagga004@ucr.edu>
2024-08-19 01:50:21 +0530

    loop to render multiple data

--- create new app/vite-project/src/App.tsx
+++ create new app/vite-project/src/App.tsx
@@ -33,14 +33,14 @@ function App() {
 				<section id="core-concepts">
 					<h2>Core Concepts</h2>
 					<ul>
-						<CoreConcept
-							title={CORE_CONCEPTS[0].title}
-							description={CORE_CONCEPTS[0].description}
-							image={CORE_CONCEPTS[0].image}
-						/>
-						<CoreConcept {...CORE_CONCEPTS[1]} />
-						<CoreConcept {...CORE_CONCEPTS[2]} />
-						<CoreConcept {...CORE_CONCEPTS[3]} />
+						{CORE_CONCEPTS.map((concept) => (
+							<CoreConcept
+								key={concept.title}
+								title={concept.title}
+								description={concept.description}
+								image={concept.image}
+							/>
+						))}
 					</ul>
 				</section>
 				<section id="examples">
